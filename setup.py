8729f8d63317b32ff83eaf5028cff8a8cb83d868
Connor Nelson <Connor@ConnorNelson.com>
2020-04-08 18:36:40 +0000

    Fix: prevent installation from building qemu multiple times

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 import importlib.util
 from pathlib import Path
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from distutils.command.build import build as _build
+from setuptools.command.develop import develop as _develop
 
 BASE_DIR = Path(__file__).resolve().parent
 
 build_path = BASE_DIR / 'tracer' / 'qemu' / 'build.py'
 spec = importlib.util.spec_from_file_location('build', build_path)
-build = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(build)
-build.build()
+build_qemu = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(build_qemu)
+
+class build(_build):
+    def run(self):
+        self.execute(build_qemu.build, ())
+        super().run()
+
+class develop(_develop):
+    def run(self):
+        self.execute(build_qemu.build, ())
+        super().run()
 
 setup(
     name='cartprograph',
@@ -26,8 +34,13 @@ setup(
         'tracer.qemu': ['bin/*'],
     },
     install_requires=[
+        'setuptools',
         'flask',
         'flask_socketio',
         'eventlet',
-    ]
+    ],
+    cmdclass={
+        'build': build,
+        'develop': develop
+    }
 )
